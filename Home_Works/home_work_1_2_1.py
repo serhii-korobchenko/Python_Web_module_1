9e171c02e002fbc129e36c420b87fa820264ee1d
Serhii Korobchenko <jchild2008@gmail.com>
2022-09-22 19:22:52 +0300

    220922

--- Home_Works/home_work_1_2_1.py
+++ Home_Works/home_work_1_2_1.py
@@ -21,9 +21,12 @@ class Meta(type):
  
         print("cls.class_number_call_", cls.class_number)
         return super().__call__(*args, **kwargs)
+
+
         
 
 Meta.children_number = 0
+
 
 class Cls1(metaclass=Meta):
     def __init__(self, data):
@@ -34,7 +37,18 @@ class Cls2(metaclass=Meta):
     def __init__(self, data):
         self.data = data
 
-assert (Cls1.class_number, Cls2.class_number) == (0, 1)
-a, b = Cls1(''), Cls2('')
-assert (a.class_number, b.class_number) == (0, 1)
+# Cls1.class_number == 0,  Cls2.class_number == 1
 
+print("Cls1.class_number = ", Cls1.class_number)
+
+print("Cls2.class_number = ", Cls2.class_number)
+
+assert (Cls1.class_number, Cls2.class_number) == (0, 1) #should be True
+
+a, b = Cls1(''), Cls2('')
+
+#a.class_number == 0, # b.class_number == 1, 
+
+print("a.class_number = ", a.class_number)
+print("b.class_number = ", b.class_number)
+assert (a.class_number, b.class_number) == (0, 1)   #should be True
